ab2608f9c35dcd17884da76981d73aca97fe71dc
rockhero1234 <149141736+rockhero1234@users.noreply.github.com>
2024-09-28 14:32:41 +0530

    Add files via upload

---
+++ build.gradle.kts
@@ -0,0 +1,86 @@
+import com.lagradost.cloudstream3.gradle.CloudstreamExtension 
+import com.android.build.gradle.BaseExtension
+
+buildscript {
+    repositories {
+        google()
+        mavenCentral()
+        maven("https://jitpack.io")
+    }
+
+    dependencies {
+        classpath("com.android.tools.build:gradle:7.0.4")
+        // Cloudstream gradle plugin which makes everything work and builds plugins
+        classpath("com.github.recloudstream:gradle:-SNAPSHOT")
+        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.9.23")
+    }
+}
+
+allprojects {
+    repositories {
+        google()
+        mavenCentral()
+        maven("https://jitpack.io")
+    }
+}
+
+fun Project.cloudstream(configuration: CloudstreamExtension.() -> Unit) = extensions.getByName<CloudstreamExtension>("cloudstream").configuration()
+fun Project.android(configuration: BaseExtension.() -> Unit) = extensions.getByName<BaseExtension>("android").configuration()
+
+subprojects {
+    apply(plugin = "com.android.library")
+    apply(plugin = "kotlin-android")
+    apply(plugin = "com.lagradost.cloudstream3.gradle")
+
+    cloudstream {
+        // compatible with other git hosting services, like gitlab, gitDab, codeBerg
+        setRepo(System.getenv("GITHUB_REPOSITORY") ?: "https://github.com/phisher98/HindiProviders")
+    }
+
+    android {
+
+        defaultConfig {
+            minSdk = 21
+            compileSdkVersion(33)
+            targetSdk = 33
+        }
+
+        compileOptions {
+            isCoreLibraryDesugaringEnabled = true
+            sourceCompatibility = JavaVersion.VERSION_1_8
+            targetCompatibility = JavaVersion.VERSION_1_8
+        }
+
+        tasks.withType<org.jetbrains.kotlin.gradle.tasks.KotlinCompile> {
+            kotlinOptions {
+                jvmTarget = "1.8" // Required
+                // Disables some unnecessary features
+                freeCompilerArgs = freeCompilerArgs +
+                        "-Xno-call-assertions" +
+                        "-Xno-param-assertions" +
+                        "-Xno-receiver-assertions"
+            }
+        }
+    }
+
+    dependencies {
+        val apk by configurations
+        val implementation by configurations
+
+        // Stubs for all Cloudstream classes
+        //noinspection GradleDeprecatedConfiguration
+        apk("com.lagradost:cloudstream3:pre-release")
+        implementation(kotlin("stdlib")) // adds standard kotlin features, like listOf, mapOf etc
+        implementation("com.github.Blatzar:NiceHttp:0.4.11") // http library
+        implementation("org.jsoup:jsoup:1.18.1") // html parser
+        implementation("com.android.tools:desugar_jdk_libs:2.1.1")
+        implementation("com.fasterxml.jackson.module:jackson-module-kotlin:2.16.1")
+        implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.8.1")
+        implementation("com.faendir.rhino:rhino-android:1.6.0")
+        implementation("me.xdrop:fuzzywuzzy:1.4.0")
+    }
+}
+
+task<Delete>("clean") {
+    delete(rootProject.buildDir)
+}
